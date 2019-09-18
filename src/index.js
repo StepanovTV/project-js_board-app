716c9810e8a8242b106acc726571b6720ab4f05c
taras <stvstudio.com.ua@gmail.com>
2019-09-18 09:44:57 +0300

    init dev

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import services from './services';
+// CSS
+import 'material-design-icons/iconfont/material-icons.css';
+import 'pnotify/dist/PNotifyBrightTheme.css';
+import 'basiclightbox/dist/basicLightbox.min.css';
+import './styles.css';
+
+services.success('DONE', 'Все работает!!!');
---
+++ src/services/index.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import PNotify from 'pnotify/dist/es/PNotify';
+import 'pnotify/dist/es/PNotifyButtons';
+import 'pnotify/dist/es/PNotifyStyleMaterial';
+import * as basicLightbox from 'basiclightbox';
+import infiniteScroll from 'infinite-scroll';
+PNotify.defaults.styling = 'material';
+PNotify.defaults.icons = 'material';
+
+export default {
+  axios: axios,
+  PNotify: PNotify,
+  basicLightbox: basicLightbox,
+  infiniteScroll: infiniteScroll,
+  image: [],
+  product: null,
+
+
+  //Методы для всплывающих оповещений... 
+  notice(title, text) {
+    this.PNotify.notice({
+      title: title,
+      text: text,
+    });
+  },
+  info(title, text) {
+    this.PNotify.info({
+      title: title,
+      text: text,
+    });
+  },
+  success(title, text) {
+    this.PNotify.success({
+      title: title,
+      text: text,
+    });
+  },
+  error(title, text) {
+    this.PNotify.error({
+      title: title,
+      text: text,
+    });
+  },
+
+ 
+
+};
